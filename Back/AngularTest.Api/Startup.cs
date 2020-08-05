75787041c6fe17936f1d7447ce5a1fa14585811c
Stéphane DUFFAUD <stephane.duffaud@gmail.com>
2020-08-05 14:44:28 +0200

    Fix iisexpress

--- Back/AngularTest.Api/Startup.cs
+++ Back/AngularTest.Api/Startup.cs
@@ -3,6 +3,7 @@ using AngularTest.Api.Persistence;
 using AngularTest.Api.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.IISIntegration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -22,6 +23,7 @@ namespace AngularTest.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(IISDefaults.AuthenticationScheme);
             services.AddControllers();
             services.AddDbContext<MainDbContext>(contextOptions =>
             {
